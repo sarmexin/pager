37d0d2092bdcc611074499a7dfdde3651f19f564
sarmexin <sarmexin@gmail.com>
2023-09-06 21:36:03 +0300

    Initial commit

---
+++ src/test/java/com/sarmexin/postman/PagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sarmexin.postman;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
